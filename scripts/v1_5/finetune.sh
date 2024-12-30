236249b636fb640d9aa29fcdfb07c5a15dcc2ea7
Weizhi Wang <wangweizhihy@gmail.com>
2024-12-30 17:14:42 -0500

    ensure float16 vision encoder during inference

--- scripts/v1_5/finetune.sh
+++ scripts/v1_5/finetune.sh
@@ -7,29 +7,29 @@
 # /mnt/raid5/weizhi/llava/sharegpt4v_mix665k_cap23k_coco-ap9k_lcs3k_sam9k_div2k_checked.json
 
 deepspeed --num_gpus=8 llava/train/train_mem.py  \
-    --model_name_or_path /mnt/raid5/weizhi/models/Meta-Llama-3.1-8B \
+    --model_name_or_path /mnt/raid5/weizhi/models/Qwen2.5-7B-Instruct \
     --deepspeed ./scripts/zero3.json \
-    --version v3 \
-    --data_path /mnt/raid5/weizhi/llava/final_data/llava_video_mix.json \
-    --image_folder /mnt/raid5/weizhi/llava/data/ \
+    --version qwen_instruct \
+    --data_path /mnt/raid5/weizhi/data/llava/final_data/llava_video_mix.json \
+    --image_folder /mnt/raid5/weizhi/data/llava/images/ \
     --vision_tower google/siglip-so400m-patch14-384 \
-    --pretrain_mm_mlp_adapter /mnt/raid5/weizhi/checkpoints/lavia-llama-3.1-pretrain-siglip-g-384-aapool-144/mm_projector.bin \
+    --pretrain_mm_mlp_adapter /mnt/raid5/weizhi/checkpoints/lavia-qwen-2.5-instruct-pretrain-siglip-g-384-aapool-64/mm_projector.bin \
     --mm_projector_type aapool_mlp \
     --mm_vision_select_layer -1 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
-    --mm_num_image_tokens 144 \
+    --mm_num_image_tokens 64 \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir /mnt/raid5/weizhi/checkpoints/lavia_video_llama_3.1_8b \
+    --output_dir /mnt/raid5/weizhi/checkpoints/LLaVA-Video-Qwen-2.5-7B \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 2 \
+    --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 8 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 50000 \
+    --save_steps 2000 \
     --save_total_limit 1 \
     --learning_rate 2e-5 \
     --weight_decay 0. \
